a4c2436b595f1d203f34931ca6cc318d08f00c26
kevinvillajim <kevinvillajim@hotmail.com>
2024-09-09 23:06:50 -0500

    Add components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,25 @@
-export default function Button({link, title, text, dark, onClick}) {
-	return (
-		<>
-			<div onClick={onClick}>
-				<a
-					href={link}
-					title={title}
-					className={dark === true ? "button-glow-dark" : "button-glow"}
-				>
-					{text}
-				</a>
-			</div>
-		</>
-	);
+import propTypes from "prop-types";
+
+export default function Button({ link, title, text, dark, onClick }) {
+  return (
+    <>
+      <div onClick={onClick}>
+        <a
+          href={link}
+          title={title}
+          className={dark === true ? "button-glow-dark" : "button-glow"}
+        >
+          {text}
+        </a>
+      </div>
+    </>
+  );
 }
+
+Button.propTypes = {
+  link: propTypes.string,
+  title: propTypes.string,
+  text: propTypes.string,
+  dark: propTypes.bool,
+  onClick: propTypes.func,
+};
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	base: "https://kevinvillajim.github.io/delFrutas/",
+  plugins: [react()],
+  // base: "https://kevinvillajim.github.io/delFrutas/",
 });
